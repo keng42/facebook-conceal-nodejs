cafcb5904acfa6180fb12927f4ceb04f76a69882
keng42 <me@keng42.com>
2020-07-22 16:46:03 +0800

    feat: update project config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,30 @@
 module.exports = {
-  extends: 'airbnb-base',
-  plugins: [
-    'import',
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  ignorePatterns: ['*.txt', '/lib', '/test/lib'],
+  include: ['src', 'test'],
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.js', '*.mjs'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
   ],
-  env: {
-    browser: true,
-    node: true,
-  },
-  globals: {
-    describe: true,
-    it: true,
-    Erorr: true,
-  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   rules: {
-    'import/no-unresolved': [2, {
-      commonjs: false,
-      amd: true,
-    }],
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: true,
-    }],
-    'no-console': 0,
-    'no-param-reassign': ['error', {
-      props: false,
-    }],
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/class-name-casing': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
 };
